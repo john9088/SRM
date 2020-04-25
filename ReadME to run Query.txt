cf9dd1c7cea52eef8c1456ee9db597c2a7d8684b
Jason Britto <35361130+john9088@users.noreply.github.com>
2020-04-25 19:24:48 +0530

    Displayed Data in from the BackEnd to Front end

---
+++ ReadME to run Query.txt
@@ -0,0 +1,3 @@
+To execute the query open CMD and run "python manage.py shell"
+Then import the models by "from accounts.models import *"
+Then execute the query "Check accounts/queryDoc.py for example"
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
 # Create your views here.
 def home(request):
-	return render(request, 'accounts/dashboard.html')
+	orders = Order.objects.all()
+	customers = Customer.objects.all()
+	total_customers = customers.count()
+	total_orders = orders.count()
+
+	orders_delivered = orders.filter(status = 'Delivered').count()
+	orders_pending = orders.filter(status = 'Pending').count()
+
+	container = {'orders':orders , 'customers': customers,
+	'total_orders':total_orders,'total_customers':total_customers,
+	'orders_delivered':orders_delivered,'orders_pending':orders_pending}
+	
+	return render(request, 'accounts/dashboard.html',container)
 
 def customer(request):
+	
 	return render(request, 'accounts/customer.html')
 
 def products(request):
-	return render(request, 'accounts/products.html')
+	products = Product.objects.all()
+	return render(request, 'accounts/products.html',{'products':products})
